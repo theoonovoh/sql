3ccf0a6cad4fabd10192b6041ab38a56906c1175
Theophilus Onovoh <theophilus.onovoh@kellogg.northwestern.edu>
2025-01-16 03:00:43 +0000

    completed this lab

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -1,4 +1,9 @@
 -- How many lifetime hits does Barry Bonds have?
+select SUM(stats.hits) 
+from players
+Inner join stats on players.id = stats.player_id
+where first_name = 'Barry' 
+and Last_name = "Bonds"
 
 -- Expected result:
 -- 2935
