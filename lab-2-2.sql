43aa6af93953cd983997e9459ff422138a87e669
Theophilus Onovoh <theophilus.onovoh@kellogg.northwestern.edu>
2025-01-16 01:50:05 +0000

    completed lab2

--- lab-2-2.sql
+++ lab-2-2.sql
@@ -1,6 +1,8 @@
 -- What are the teams (and years) that have played at 
 -- Guaranteed Rate Field?
 
+Select year, name,park from teams where park = "Guaranteed Rate Field"
+
 -- Expected result:
 --
 -- +------+-------------------+
--- lab-2-3.sql
+++ lab-2-3.sql
@@ -1,7 +1,9 @@
 -- What was the last team (and year) to play at U.S. Cellular Field?
 
+Select year, name from teams where park = "U.S. Cellular Field" 
+order by year DESC limit 1
 -- Expected result:
---
+-- 
 -- +------+-------------------+
 -- | 2017 | Chicago White Sox |
 -- +------+-------------------+
